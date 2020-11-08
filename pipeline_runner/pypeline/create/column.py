5a8aae8119eb2b178a68203a4259975428ed679b
carloxluca91 <carloni.luca91@gmail.com>
2020-11-08 20:49:55 +0100

    keep working on create step stuff ...

--- pipeline_runner/pypeline/create/column.py
+++ pipeline_runner/pypeline/create/column.py
@@ -1,20 +1,21 @@
 import logging
-from datetime import datetime, date
-from typing import List, Union, Dict, Any, Tuple
+from typing import List, Union, Dict, Any
 
 import numpy as np
 from pyspark.sql import SparkSession
 
 from pypeline.abstract import AbstractPipelineElement
-from pypeline.create.metadata import RandomColumnMetadata, DateOrTimestampMetadata
-from utils.time import TimeUtils
+from pypeline.create.metadata import RandomColumnMetadata, DateOrTimestampMetadata, RangeColumnMetadata
 
-T = Union[DateOrTimestampMetadata, RandomColumnMetadata]
+T = Union[DateOrTimestampMetadata,
+          RandomColumnMetadata,
+          RangeColumnMetadata]
 
 _METADATA_TYPE = {
 
     "date": DateOrTimestampMetadata,
-    "timestamp": DateOrTimestampMetadata
+    "timestamp": DateOrTimestampMetadata,
+    "range": RangeColumnMetadata
 }
 
 
@@ -38,7 +39,7 @@ class TypedColumn(AbstractPipelineElement):
         self._nullable = nullable
         self._nullable_probability = nullable_probability
         self._column_number = column_number
-        self._typed_metadata: T = _METADATA_TYPE.get(self._column_type, RandomColumnMetadata).from_dict(metadata)
+        self._metadata = metadata
 
     @property
     def column_type(self) -> str:
@@ -54,66 +55,27 @@ class TypedColumn(AbstractPipelineElement):
 
     def create(self, number_of_records: int, spark_session: SparkSession) -> List[Any]:
 
-        typed_metadata = self._typed_metadata
-        if isinstance(typed_metadata, DateOrTimestampMetadata):
+        if self.column_type in ["date", "timestamp"]:
 
-            is_date = typed_metadata.is_date
-            lower_bound = typed_metadata.lower_bound
-            time_delta = typed_metadata.time_delta
+            typed_metadata = DateOrTimestampMetadata.from_dict(self._metadata)
+            random_data = typed_metadata.create_data(number_of_records)
 
-            # Define a lambda expression depending on column type
-            date_or_datetime_lambda = (lambda x: (lower_bound + time_delta * x).date()) if is_date else (lambda x: (lower_bound + time_delta * x))
-            random_data: List[Union[date, datetime]] = list(map(date_or_datetime_lambda, self._rng.random_sample(number_of_records)))
+        elif self.column_type == "range":
 
-            # If dates (or timestamps) must be converted to strings
-            if typed_metadata.as_string:
-
-                java_output_format: str = typed_metadata.java_output_format
-                self._logger.info(f"Converting {self._column_type}s within column '{self.name}' to string with format '{java_output_format}'")
-                random_data: List[str] = list(map(lambda x: TimeUtils.format(x, java_output_format), random_data))
-
-                # Check if data have to be corrupted with wrong time formats
-                corrupt_flag = typed_metadata.corrupt_flag
-                corrupt_prob = typed_metadata.corrupt_prob
-                java_corrupt_format = typed_metadata.java_corrupt_format
-
-                if corrupt_flag and corrupt_prob and java_corrupt_format:
-
-                    # If so, some dates (or timestamps), now as strings, must be converted back and forth in order to modify their string format
-                    self._logger.info(f"Corrupting {self._column_type}s within column '{self.name}' "
-                                      f"with prob {typed_metadata.corrupt_prob}, "
-                                      f"format = '{typed_metadata.java_corrupt_format}')")
-
-                    corruption_probabilities = self._rng.choice([0, 1], number_of_records, p=[1 - corrupt_prob, corrupt_prob])
-
-                    # Function for modifying date (or timestamp) string format
-                    def corrupt_lambda(t: Tuple[str, int]) -> str:
-
-                        dt_or_ts, prob = t[0], t[1]
-                        return TimeUtils.format(TimeUtils.to_datetime(dt_or_ts, java_output_format), java_corrupt_format) if prob == 1 \
-                            else dt_or_ts
-
-                    random_data: List[str] = list(map(corrupt_lambda, zip(random_data, corruption_probabilities)))
+            typed_metadata = RangeColumnMetadata.from_dict(self._metadata)
+            random_data = typed_metadata.create_data(number_of_records)
 
         else:
 
-            # If values to be picked (and their probabilities) are embedded within column infos
-            if typed_metadata.has_embedded_data:
-
-                random_data = self._rng.choice(typed_metadata.embedded_values, size=number_of_records, p=typed_metadata.embedded_probs)
-
-            else:
-
-                # Retrieve values to be picked (and their probabilities) from Hive table
-                # TODO
-                random_data = []
+            typed_metadata = RandomColumnMetadata.from_dict(self._metadata)
+            random_data = typed_metadata.create_data(number_of_records, spark_session)
 
         if self._nullable:
 
             nullable_probability = self._nullable_probability
             self._logger.info(f"Corrupting data of column '{self.name}' with some None values using probability = {nullable_probability}")
             none_probabilities = self._rng.choice([0, 1], len(random_data), p=[1 - nullable_probability, nullable_probability])
-            return [original_datum if prob == 0 else None for original_datum, prob in zip(random_data, none_probabilities)]
+            return [None if prob else datum for datum, prob in zip(random_data, none_probabilities)]
 
         else:
 
--- pipeline_runner/pypeline/create/metadata.py
+++ pipeline_runner/pypeline/create/metadata.py
@@ -1,11 +1,58 @@
-from datetime import datetime, timedelta
-from typing import Dict
+import logging
+from abc import abstractmethod, ABC
+from datetime import datetime, timedelta, date
+from typing import Dict, List, Union, Tuple, Any
+
+import numpy as np
+from pyspark.sql import SparkSession, functions, Row
 
 from pypeline.abstract import AbstractJsonElement
 from utils.time import TimeUtils
 
+T = Union[date, datetime, str, int]
 
-class DateOrTimestampMetadata(AbstractJsonElement):
+
+class MetadataUtils:
+
+    @staticmethod
+    def get_or_else(dict_: Dict, key: str, default):
+
+        return dict_.get(key, default) if dict_ else default
+
+
+class BaseMetadata(AbstractJsonElement, ABC):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self._logger = logging.getLogger(__name__)
+        self._rng = np.random.RandomState()
+
+
+class AbstractMetadata(BaseMetadata, ABC):
+
+    def __init__(self):
+
+        super().__init__()
+
+    @abstractmethod
+    def create_data(self, number_of_records: int) -> List[T]:
+        pass
+
+
+class AbstractMetadataPlusSparkSession(BaseMetadata, ABC):
+
+    def __init__(self):
+
+        super().__init__()
+
+    @abstractmethod
+    def create_data(self, number_of_records: int, spark_session: SparkSession) -> List[T]:
+        pass
+
+
+class DateOrTimestampMetadata(AbstractMetadata):
 
     def __init__(self,
                  lower_bound: str,
@@ -29,54 +76,64 @@ class DateOrTimestampMetadata(AbstractJsonElement):
         self._time_delta: timedelta = self._upper_bound_dtt - self._lower_bound_dtt
 
         self._as_string = as_string
+        with MetadataUtils.get_or_else as get_or_else:
 
-        def get_or_else(dict_: Dict, key: str, default):
+            self._java_output_format = get_or_else(as_string_info, "outputFormat", default_format)
+            self._corrupt_flag = get_or_else(as_string_info, "corruptFlag", False)
+            self._corrupt_probability = get_or_else(as_string_info, "corruptProb", 0)
+            self._java_corrupt_format = get_or_else(as_string_info, "corruptFormat", TimeUtils.java_default_corrupt_format())
 
-            return dict_.get(key, default) if dict_ else default
+    def create_data(self, number_of_records: int) -> List[T]:
 
-        self._java_output_format = get_or_else(as_string_info, "outputFormat", default_format)
-        self._corrupt_flag = get_or_else(as_string_info, "corruptFlag", False)
-        self._corrupt_probability = get_or_else(as_string_info, "corruptProb", 0)
-        self._java_corrupt_format = get_or_else(as_string_info, "corruptFormat", TimeUtils.java_default_corrupt_format())
+        is_date = self._is_date
+        lower_bound = self._lower_bound_dtt
+        time_delta = self._time_delta
 
-    @property
-    def lower_bound(self) -> datetime:
-        return self._lower_bound_dtt
+        # Define a lambda expression depending on column type
+        date_or_datetime_lambda = (lambda x: (lower_bound + time_delta * x).date()) if is_date else (lambda x: (lower_bound + time_delta * x))
+        random_data: List[Union[date, datetime]] = list(map(date_or_datetime_lambda, self._rng.random_sample(number_of_records)))
 
-    @property
-    def upper_bound(self) -> datetime:
-        return self._upper_bound_dtt
+        # If dates (or timestamps) must be converted to strings
+        if self._as_string:
 
-    @property
-    def time_delta(self) -> timedelta:
-        return self._time_delta
+            java_output_format: str = self._java_output_format
+            self._logger.info(f"Converting {'date' if is_date else 'timestamp'}(s) to string with format '{java_output_format}'")
+            random_data: List[str] = list(map(lambda x: TimeUtils.format(x, java_output_format), random_data))
 
-    @property
-    def is_date(self) -> bool:
-        return self._is_date
+            # Check if data have to be corrupted with wrong time formats
+            corrupt_flag = self._corrupt_flag
+            corrupt_prob = self._corrupt_probability
+            java_corrupt_format = self._java_corrupt_format
 
-    @property
-    def as_string(self) -> bool:
-        return self._as_string
+            if corrupt_flag and corrupt_prob and java_corrupt_format:
 
-    @property
-    def java_output_format(self) -> str:
-        return self._java_output_format
+                # If so, some dates (or timestamps), now as strings, must be converted back and forth in order to modify their string format
+                self._logger.info(f"Corrupting {'date' if is_date else 'timestamp'}(s) with "
+                                  f"prob {corrupt_prob}, "
+                                  f"format = '{java_corrupt_format}')")
 
-    @property
-    def corrupt_flag(self) -> bool:
-        return self._corrupt_flag
+                corruption_probabilities = self._rng.choice([0, 1], number_of_records, p=[1 - corrupt_prob, corrupt_prob])
 
-    @property
-    def corrupt_prob(self) -> float:
-        return self._corrupt_probability
+                # Function for modifying date (or timestamp) string format
+                def corrupt_lambda(t: Tuple[str, int]) -> str:
+                    dt_or_ts, prob = t[0], t[1]
+                    return TimeUtils.format(TimeUtils.to_datetime(dt_or_ts, java_output_format), java_corrupt_format) if prob == 1 \
+                        else dt_or_ts
 
-    @property
-    def java_corrupt_format(self) -> str:
-        return self._java_corrupt_format
+                random_data: List[str] = list(map(corrupt_lambda, zip(random_data, corruption_probabilities)))
+
+            else:
+
+                self._logger.info(f"No corruption specified for column {'TODO'}")
+
+        else:
+
+            self._logger.info(f"Returning data as list of {'date' if is_date else 'timestamp'}(s)")
+
+        return random_data
 
 
-class RandomColumnMetadata(AbstractJsonElement):
+class RandomColumnMetadata(AbstractMetadataPlusSparkSession):
 
     def __init__(self,
                  data_origin: str,
@@ -85,24 +142,80 @@ class RandomColumnMetadata(AbstractJsonElement):
         super().__init__()
 
         self._has_embedded_data = data_origin.lower() == "embedded"
+        with MetadataUtils.get_or_else as get_or_else:
 
-        def get_or_else(dict_: Dict, key: str, default):
+            self._embedded_values = get_or_else(data_info, "values", [])
+            len_embedded_values = len(self._embedded_values)
+            default_prob = 1 if len_embedded_values == 0 else [1/len_embedded_values] * len_embedded_values
+            self._embedded_probs = get_or_else(data_info, "probs", default_prob)
+            self._db_name = get_or_else(data_info, "dbName", None)
+            self._table_name = get_or_else(data_info, "tableName", None)
+            self._pipeline_name = get_or_else(data_info, "pipelineName", None)
+            self._dataframe_id = get_or_else(data_info, "dataframeId", None)
+            self._column_name = get_or_else(data_info, "columnName", None)
 
-            return dict_.get(key, default) if dict_ else default
+    def create_data(self, number_of_records: int, spark_session: SparkSession) -> List[Any]:
 
-        self._embedded_values = get_or_else(data_info, "values", [])
-        len_embedded_values = len(self._embedded_values)
-        default_prob = 1 if len_embedded_values == 0 else [1/len_embedded_values] * len_embedded_values
-        self._embedded_probs = get_or_else(data_info, "probs", default_prob)
+        values: List[Any]
+        probs: List[Any]
 
-    @property
-    def has_embedded_data(self) -> bool:
-        return self._has_embedded_data
+        if self._has_embedded_data:
 
-    @property
-    def embedded_values(self):
-        return self._embedded_values
+            # If values to be picked (and their probabilities) are embedded within column infos
+            values = self._embedded_values
+            probs = self._embedded_probs
 
-    @property
-    def embedded_probs(self):
-        return self._embedded_probs
+        else:
+
+            # Otherwise, retrieve values to be picked (and their probabilities) from Hive table
+            pipeline_name_column = "pipeline_name"
+            dataframe_id_column = "dataframe_id"
+            column_name_column = "column_name"
+
+            db_name: str = self._db_name
+            table_name: str = self._table_name
+            pipeline_name: str = self._pipeline_name
+            dataframe_id: str = self._dataframe_id
+            column_name: str = self._column_name
+
+            related_to_info = f"({pipeline_name_column} = '{pipeline_name}', " \
+                              f"{dataframe_id_column} = '{dataframe_id}', " \
+                              f"{column_name_column} = '{column_name}')"
+
+            full_table_name: str = f"{db_name}.{table_name}"
+            self._logger.info(f"Trying to scan table '{full_table_name}' in order to detect some records related to {related_to_info}")
+
+            values_and_probs: List[Row] = spark_session.table(full_table_name) \
+                .filter((functions.col(pipeline_name_column) == pipeline_name) &
+                        (functions.col(dataframe_id_column) == dataframe_id) &
+                        (functions.col(column_name_column) == column_name)) \
+                .select("value", "probability") \
+                .collect()
+
+            if len(values_and_probs) == 0:
+                value_error_msg: str = f"Unable to detect records from table '{full_table_name}' related to {related_to_info}"
+
+                raise ValueError(value_error_msg)
+
+            self._logger.info(f"Identified {len(values_and_probs)} row(s) related to {related_to_info}")
+
+            values = [r["value"] for r in values_and_probs]
+            probs = [r["probability"] for r in values_and_probs]
+
+        return self._rng.choice(values, size=number_of_records, p=probs)
+
+
+class RangeColumnMetadata(AbstractMetadata):
+
+    def __init__(self,
+                 start: int = 1,
+                 step: int = 1):
+
+        super().__init__()
+
+        self._start = start
+        self._step = step
+
+    def create_data(self, number_of_records: int) -> List[T]:
+
+        return list(range(self._start, step=self._step))
